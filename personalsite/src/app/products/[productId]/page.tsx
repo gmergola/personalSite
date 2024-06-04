a132444050bf38d8a60bcd5b08123b09e796986d
Genna Mergola <gennamergola@gmail.com>
2024-06-04 17:22:36 -0400

    add random nextjs code and MUI

---
+++ personalsite/src/app/products/[productId]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductDetails({ params }: { params: any }) {
+  return <h1>Product details {params.productId}</h1>;
+}
---
+++ personalsite/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,3 @@
+export default function ReviewDetails({ params }: { params: any }) {
+  return <h1>Review details {params.reviewId}</h1>;
+}
